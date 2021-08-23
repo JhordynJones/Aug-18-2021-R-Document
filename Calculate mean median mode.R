ef05df05545f7e477858ec8d00616a5f593dda24
JhordynJones <89275296+JhordynJones@users.noreply.github.com>
2021-08-23 15:31:48 -0400

    Add files via upload

---
+++ Calculate mean median mode.R
@@ -0,0 +1,21 @@
+#Author: Jhordyn Jones, Date: Aug 18, 2021, Purpose: Calculate mean, median, mode
+
+# Dummy data
+
+variable1 <- c(1, 2, 3, 4, 5)
+
+# calculate the mean/average of variable1
+
+mean <- mean(variable1)
+#3
+
+# calculate median value
+
+median <- median(variable1)
+#3
+
+# calculate mode value
+variable2 <- c(1, 2, 3, 4, 5, 2) # unimodel data, just one mode which is "2"
+
+# Install the external packages in R
+install.packages("modest")
